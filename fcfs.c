7e2d87d1b37f0d743fdf7b8c4240ea1a5b892239
annmariya23 <mariyaannsam23@gmail.com>
2026-02-28 22:12:07 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -25,21 +25,26 @@ int main() {
         for (int j = 0; j < n - i - 1; j++) {
 
             if (p[j].arrival > p[j + 1].arrival ||
-                 (p[j].arrival == p[j + 1].arrival &&
-                 p[j].index > p[j + 1].index)) {
+                (p[j].arrival == p[j + 1].arrival &&
+                p[j].index > p[j + 1].index)) {
 
-                Process temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
+                    Process temp = p[j];
+                    p[j] = p[j + 1];
+                    p[j + 1] = temp;
             }
         }
     }
 
-    int current_time = 0;
-    int total_wt = 0;
-    int total_tat = 0;
+    int current_time = p[0].arrival;
+    p[0].waiting = 0;
+    p[0].turnaround = p[0].burst;
 
-    for (int i = 0; i < n; i++) {
+    int total_wt = p[0].waiting;
+    int total_tat = p[0].turnaround;
+
+    current_time += p[0].burst;
+
+    for (int i = 1; i < n; i++) {
 
         if (current_time < p[i].arrival) {
             current_time = p[i].arrival;
@@ -65,7 +70,6 @@ int main() {
     }
 
     printf("Average Waiting Time: %.2f\n", (float)total_wt / n);
-    printf("Average Turnaround Time: %.2f", (float)total_tat / n);
-
+    printf("Average Turnaround Time: %.2f\n", (float)total_tat / n);
     return 0;
 }
