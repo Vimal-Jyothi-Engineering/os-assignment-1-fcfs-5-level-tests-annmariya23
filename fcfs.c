b9a1525452a61dd2e0683bda18e0d33c562d1f62
annmariya23 <mariyaannsam23@gmail.com>
2026-02-28 21:25:48 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -7,7 +7,6 @@ typedef struct {
     int burst;
     int waiting;
     int turnaround;
-    int index;
 } Process;
 
 int main() {
@@ -18,21 +17,18 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
-        p[i].index = i;
     }
 
     for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
-
-        if (p[j].arrival > p[j + 1].arrival ||
-           (p[j].arrival == p[j + 1].arrival && p[j].index > p[j + 1].index)) {
-
-            Process temp = p[j];
-            p[j] = p[j + 1];
-            p[j + 1] = temp;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (p[j].arrival > p[j + 1].arrival) {
+                Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
         }
     }
-}
+
     int current_time = 0;
     int total_wt = 0;
     int total_tat = 0;
